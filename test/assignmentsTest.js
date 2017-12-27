a0ac08df86e374cc4c8a0d4fff665b4c15d11702
martlandh <HarryMartland@Rentalcars.com>
2017-12-27 11:52:00 +0000

    added test to make sure assignments are uppercase

--- test/assignmentsTest.js
+++ test/assignmentsTest.js
@@ -8,5 +8,11 @@ describe('assignments', function () {
 
     it('should not find jamaca', function () {
         assert.ifError(assignments['JAMACA'])
-    })
+    });
+    
+    it('should all be uppercase', function () {
+        Object.keys(assignments).forEach(function (key) {
+            assert.equal(key.toUpperCase(), key);
+        })
+    });
 });
